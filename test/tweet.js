18023631e6a2f03cdb5d6c1fb4a9946df23e091d
Pakanon Pantisawat <pakanon.pk@gmail.com>
2019-10-30 16:41:40 +0700

    Edit test

--- test/tweet.js
+++ test/tweet.js
@@ -15,8 +15,8 @@ describe("Initial state", function() {
           expect(error).to.be.null;
           expect(response).to.have.status(200);
           expect(response.body).to.have.lengthOf(0);
+          done();
         });
-      done();
     });
   });
 });
@@ -24,7 +24,7 @@ describe("Initial state", function() {
 describe("Normal state", function() {
   const agent = chai.request.agent(app);
   describe("POST /", function() {
-    it("should add new tweets", function(done) {
+    it("should have created response", function(done) {
       agent
         .post("/")
         .send({
@@ -33,19 +33,23 @@ describe("Normal state", function() {
         })
         .then(response => {
           expect(response).to.have.status(201);
-          agent.get("/").then(response => {
-            expect(response).to.have.status(200);
-            expect(response.body).to.be.lengthOf(1);
-            done();
-          });
+          done();
         })
-        .catch(error => {
-          throw error;
-        });
+        .catch(done);
+    });
+    it("should have 1 tweet", function(done) {
+      agent
+        .get("/")
+        .then(response => {
+          expect(response).to.have.status(200);
+          expect(response.body).to.be.lengthOf(1);
+          done();
+        })
+        .catch(done);
     });
   });
   describe("PUT /", function() {
-    it("should edit tweet content", function(done) {
+    it("should response with success", function(done) {
       agent
         .put("/0")
         .send({
@@ -54,45 +58,63 @@ describe("Normal state", function() {
         })
         .then(response => {
           expect(response).to.have.status(200);
-          agent.get("/").then(response => {
-            expect(response).to.have.status(200);
-            expect(response.body[0].content).to.be.equal("AHH");
-            expect(response.body[0].username).to.be.equal("pknn");
-            done();
-          });
+          done();
         })
-        .catch(error => {
-          throw error;
-        });
+        .catch(done);
     });
-    it("should not edit tweet content if username is not match", function(done) {
+    it("should have content edited", function(done) {
+      agent
+        .get("/")
+        .then(response => {
+          expect(response).to.have.status(200);
+          expect(response.body[0].content).to.be.equal("AHH");
+          expect(response.body[0].username).to.be.equal("pknn");
+          done();
+        })
+        .catch(done);
+    });
+    it("should get bad request", function(done) {
       agent
         .put("/0")
         .send({ content: "OHH", username: "haha" })
         .then(response => {
           expect(response).to.have.status(400);
-          agent.get("/").then(response => {
-            expect(response).to.have.status(200);
-            expect(response.body[0].content).to.be.equal("AHH");
-            expect(response.body[0].username).to.be.equal("pknn");
-            done();
-          });
-        });
+          done();
+        })
+        .catch(done);
+    });
+    it("should not edit tweet content", function(done) {
+      agent
+        .get("/")
+        .then(response => {
+          expect(response).to.have.status(200);
+          expect(response.body[0].content).to.be.equal("AHH");
+          expect(response.body[0].username).to.be.equal("pknn");
+          done();
+        })
+        .catch(done);
     });
   });
   describe("DELETE /", function() {
-    it("should delete tweet", function(done) {
+    it("should get success", function(done) {
       agent
         .delete("/0")
         .send()
         .then(response => {
           expect(response).to.have.status(200);
-          agent.get("/").then(response => {
-            expect(response).to.have.status(200);
-            expect(response.body).to.have.lengthOf(0);
-            done();
-          });
-        });
+          done();
+        })
+        .catch(done);
+    });
+    it("should have 0 tweet", function(done) {
+      agent
+        .get("/")
+        .then(response => {
+          expect(response).to.have.status(200);
+          expect(response.body).to.have.lengthOf(0);
+          done();
+        })
+        .catch(done);
     });
     it("should get 404 when tweet not exist", function(done) {
       agent
@@ -101,7 +123,8 @@ describe("Normal state", function() {
         .then(response => {
           expect(response).to.have.status(404);
           done();
-        });
+        })
+        .catch(done);
     });
   });
 });
